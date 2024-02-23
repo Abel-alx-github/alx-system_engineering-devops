41ec6c1ac952af4d982ee377acd42d330f342b50
abel-alx-github <abeltibeso323@gmail.com>
2024-02-23 07:54:40 -0800

    task 0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Using Puppet, install flask from pip3.
 
 package {'flask':
-  ensure          => installed,
-  provider        => 'pip3',
-  install_options => ['--version', '2.1.0']
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
