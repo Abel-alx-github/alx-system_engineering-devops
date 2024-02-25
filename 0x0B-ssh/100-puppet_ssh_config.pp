305f9b5327cc828161659cf6783de778c109ad61
abel-alx-github <abeltibeso323@gmail.com>
2024-02-25 10:27:01 -0800

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,16 @@
 #  using Puppet to make changes to our configuration file
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
 
-file{'~/.ssh/shcool':
-  ensure  =>  file,
-  content =>  'HOST *
-              IdentityFile ~/.ssh/school
-              PasswordAuthentication no
-              '
-  }
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'passwordAuthentication no',
+  match => '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#IdentityFile',
+}
