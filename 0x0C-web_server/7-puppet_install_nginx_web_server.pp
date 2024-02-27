908d10e3d50f75442b8e895ebe54add8c091b021
abel-alx-github <abeltibeso323@gmail.com>
2024-02-27 11:42:36 -0800

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,15 +5,16 @@ package {
     ensure => installed,
 }
 
-file {'/var/www/html/index.nginx-debian.html':
+file {'/var/www/html/index.html':
     content => 'Hello World!',
 }
 
 file_line {'configure redirection':
     path  =>  '/etc/nginx/sites-available/default',
     after =>  'server_name _;',
-    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://github.com/Abel-alx-github;\n\t}\n",
 }
+
 
 service {'nginx':
     ensure => running,
