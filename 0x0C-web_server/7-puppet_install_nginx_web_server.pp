06a494733c7f13e8a3fad5cc9fa2d6550db56f2b
abel-alx-github <abeltibeso323@gmail.com>
2024-02-27 11:37:25 -0800

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,20 @@
 #install nginx and config 
 
-
-package { 'nginx':
-  ensure => installed,
+package {
+    'nginx':
+    ensure => installed,
 }
 
-exec { 'allow_http':
-  command     => ' /usr/sbin/ufw allow "Nginx HTTP"',
-  refreshonly => true,
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
 }
 
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => present,
-  content => 'server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    error_page 404 /my_error404.html;
-
-    root /var/www/html;
-
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-    rewrite ^/redirect_me https://github.com/Abel-alx-github permanent;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-',
-  require => [Package['nginx'], Exec['allow_http']],
-  notify  => Service['nginx'],
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
 }
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+service {'nginx':
+    ensure => running,
 }
