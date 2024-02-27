93e68ce0c4ad975b39f309de442c3e80d43687a5
abel-alx-github <abeltibeso323@gmail.com>
2024-02-27 11:34:28 -0800

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,48 +1,40 @@
 #install nginx and config 
-# Install Nginx
+
+
 package { 'nginx':
   ensure => installed,
 }
 
-# Configure Nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-    server {
-      listen 80;
-      server_name _;
-
-      location = / {
-        return 200 'Hello World!';
-      }
-
-      location = /redirect_me {
-        return 301 https://github.com/Abel-alx-github;
-      }
-
-      error_page 404 /my_error404.html;
-
-      location = /my_error404.html {
-        return 200 'Ceci n\'est pas une page';
-      }
-    }
-  ",
-  require => Package['nginx'],
+exec { 'allow_http':
+  command     => ' /usr/sbin/ufw allow "Nginx HTTP"',
+  refreshonly => true,
 }
 
-# Enable Nginx default site
 file { '/etc/nginx/sites-enabled/default':
-  ensure  => 'link',
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
+  ensure  => present,
+  content => 'server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    error_page 404 /my_error404.html;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+    rewrite ^/redirect_me https://github.com/Abel-alx-github permanent;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+',
+  require => [Package['nginx'], Exec['allow_http']],
+  notify  => Service['nginx'],
 }
 
-# Restart Nginx
 service { 'nginx':
   ensure  => running,
   enable  => true,
-  require => [
-    Package['nginx'],
-    File['/etc/nginx/sites-enabled/default'],
-  ],
+  require => Package['nginx'],
 }
