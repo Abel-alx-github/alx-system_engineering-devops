af0d0e32eceac76aba2c4d1246128cb1812b474f
abel-alx-github <abeltibeso323@gmail.com>
2024-04-09 20:05:52 +0300

    query subscribers for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" make get request to reddit api of a given subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ return no of subscribe if it is valid subreddit"""
+    if subreddit:
+        url = f'https://www.reddit.com/r/{subreddit}/about.json'
+        respond = requests.get(url)
+        sub = None
+        if (respond.status_code == 200):
+
+            sub = respond.json()['data'].get('subscribers')
+        if sub:
+            return sub
+        return 0
