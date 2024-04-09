eeee4cbf86d2c10c54cb476f20b05d78835e86aa
abel-alx-github <abeltibeso323@gmail.com>
2024-04-09 21:47:58 +0300

    print title of hot 10 posts for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""print top ten of hot posts for a given subreddit """
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """ print top ten hot posts"""
+    if subreddit:
+        headers = {
+                   'User-Agent': 'pKL2PTBBkJt0gXrFASOUYw',
+                  }
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+        respond = requests.get(url, headers=headers, allow_redirects=False,
+                               params={"limit": 10},)
+
+        if respond.status_code == 200:
+            for post in respond.json().get('data').get('children'):
+                print(post.get('data').get('title'))
+            return
+        print(None)
