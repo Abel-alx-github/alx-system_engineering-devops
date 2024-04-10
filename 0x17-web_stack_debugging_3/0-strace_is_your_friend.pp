9ad4123c273103ef425c7dfeb0b3708fe6df8bd7
abel-alx-github <abeltibeso323@gmail.com>
2024-04-10 21:49:23 +0300

    fix webstack 3 debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,5 @@
 
 exec { "let's fix":
     provider => 'shell',
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php/'
-
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
